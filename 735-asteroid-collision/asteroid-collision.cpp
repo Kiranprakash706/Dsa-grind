b435b74b4b8e3ac72cff9138b7869da919cb8326
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2025-12-31 16:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (75.53%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,83 +1,31 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> stack;
-        for (int a : asteroids) {
-            bool alive = true;
-            while (alive && !stack.empty() && stack.back() > 0 && a < 0) {
-                if (abs(stack.back()) < abs(a)) {
-                    stack.pop_back();
-                } else if (abs(stack.back()) == abs(a)) {
-                    stack.pop_back();
-                    alive = false;
-                } else {
-                    alive = false;
-                }
+    vector<int>ans;
+    ans.push_back(asteroids[0]);
+    bool isalive=false;
+    for(int i=1;i<asteroids.size();i++){
+        isalive=true;
+        while(isalive && !ans.empty() && asteroids[i]<0 && ans.back()>0){
+            if(abs(ans.back())==abs(asteroids[i])){
+                ans.pop_back();
+                isalive=false;
             }
-            if (alive) stack.push_back(a);
+            else if((abs(ans.back())>abs(asteroids[i]) )){
+    
+                isalive=false;
+
+            }
+            else{
+                ans.pop_back();
+                isalive=true;
+            }
+            
         }
-        return stack;
+        if(isalive){
+            ans.push_back(asteroids[i]);
+        }
+    }
+    return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        
-        vector<int>ans;
-        ans.push_back(asteroids[0]);
-        bool isalive=false;
-        for(int i=1;i<asteroids.size();i++){
-            isalive=true;
-            while(isalive &&!ans.empty() && ans.back()>0 && asteroids[i]<0){
-                if(abs(ans.back())!=abs(asteroids[i])){
-                int temp=max(abs(asteroids[i]), abs(ans.pop_back()));
-                ans.pop_back();
-                ans.push_back(temp)
-
-            }
-        }
-        return ans;
-    }
-};*/
