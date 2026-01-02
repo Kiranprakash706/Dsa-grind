b26edcbf02d2ae07087b0f4d524f2e80fd049d7a
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-01-02 20:09:25 +0530

    Time: 3 ms (38.98%) | Memory: 17.2 MB (36.68%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& s) {
+        stack<int >st;
+
+        for(int i=0;i<s.size();i++){
+           if(s[i] != "+" && s[i] != "-" && s[i] != "*" && s[i] != "/")
+        {
+            st.push(stoi(s[i]));
+           }
+           else{
+            int a=st.top();
+            st.pop();
+            int b=st.top();
+            st.pop();
+            if(s[i]=="+"){
+                st.push(a+b);
+            }
+            else if(s[i]=="-"){
+                st.push(b-a);
+
+            }
+            else if(s[i]=="*"){
+                st.push(a*b);
+            }
+            else {
+                st.push(b/a);
+            }
+           }
+        }
+        return st.top();
+       
+
+    }
+};
