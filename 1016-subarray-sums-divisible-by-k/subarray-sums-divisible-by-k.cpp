c1ecd57cd776bd20e6c2651d20f3203c7060e3b3
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-01-22 19:05:54 +0530

    Time: 12 ms (44.38%) | Memory: 35.8 MB (20.54%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -4,9 +4,13 @@ public:
         int sum=0;
         int res=0;
         unordered_map<int , int > mapp;
-        mapp[0]=1;
+        // mapp[0]=1;
         for(int i=0;i<nums.size();i++){
+            
            sum+=nums[i];
+           if(sum%k==0){
+                res++;
+            }
            int temp=(sum%k+k)%k;
            if(mapp.find(temp)!=mapp.end()){
             res+=mapp[temp];
