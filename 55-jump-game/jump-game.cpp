98359b420899b9f40ddaf4d0947140345f2f21d3
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-02-16 15:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (47.04%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,29 +1,17 @@
-class Solution {
+class Solution{
 public:
- vector<int>dp;
-int  helper(int ind, vector<int>&nums){
-    if(ind==nums.size()-1){
-        return true;
-    }
-    if(nums[ind]==0){
-        return false;
-    }
-    if(dp[ind]!=-1){
-        return dp[ind];
-    }
-    else{
-    bool ispossible=false;
-    for(int i=1;i<=nums[ind];i++){
-        ispossible=helper(ind+i, nums);
-        if(ispossible){
-            return dp[ind]=1;
-        }
-    }
-    return dp[ind]=0;
-    }
-}
     bool canJump(vector<int>& nums) {
-       dp.resize(nums.size()+1,-1);
-        return helper(0, nums);
+        int maxjump=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxjump){
+                return false;
+            }
+            maxjump=max(maxjump, i+nums[i]);
+            if(maxjump>=nums.size()-1){
+                return true;
+            }
+          
+        }
+        return true;
     }
 };
