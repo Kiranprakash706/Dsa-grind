c1ea89c9a5aa0432456e14d9c38a21838560b92b
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-02-19 12:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 74.9 MB (85.78%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
@@ -1,30 +1,11 @@
 class Solution {
 public:
-bool ispossible(vector<int>&nums, int mid){
-    long long carry=0;
-    for(int i=nums.size()-1;i>0;i--){
-        if(nums[i]+carry>mid){
-            carry+=nums[i]-mid;
-        }
-        else{
-            carry=0;
-        }
-    }
-    return nums[0]+carry<=mid;
-}
     int minimizeArrayValue(vector<int>& nums) {
-        int lb=0;
-        int hb=*max_element(nums.begin(), nums.end());
-        int ans=hb;
-        while(lb<=hb){
-            int mid=hb+(lb-hb)/2;
-            if(ispossible(nums, mid)){
-                ans=mid;
-                hb=mid-1;
-            }
-            else{
-                lb=mid+1;
-            }
+        long long  sum=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans=max(ans, (int )((sum+i)/(i+1)));
         }
         return ans;
     }
