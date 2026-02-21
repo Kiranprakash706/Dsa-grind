bda4ef1b9dbbad85a6ca91b256d46936753f860e
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-02-21 18:32:45 +0530

    Time: 28 ms (61.55%) | Memory: 60.5 MB (37.00%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    static bool cmd(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];  
+    }
+
+    int scheduleCourse(vector<vector<int>>& arr) {
+        sort(arr.begin(), arr.end(), cmd);
+
+        priority_queue<int> pq; 
+        int duration = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int d = arr[i][0];
+            int end = arr[i][1];
+
+            if (duration + d <= end) {
+                duration += d;
+                pq.push(d);
+            }
+            else if (!pq.empty() && pq.top() > d) {
+                duration -= pq.top(); 
+                pq.pop();
+                duration += d;         
+                pq.push(d);
+            }
+        }
+
+        return pq.size();
+    }
+};
