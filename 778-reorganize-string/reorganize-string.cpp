12b7f69c9921e8e660781fcefe96e4d930cef595
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-04-07 16:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.43%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        priority_queue<pair<int , char>>pq;
+        unordered_map<char , int >mapp;
         int n=s.length();
-       unordered_map<char, int >mapp;
-       for(int i=0;i<s.length();i++){
-        mapp[s[i]]++;
-        if(mapp[s[i]]>((n+1)/2)){
-            return "";
-        }
-       }
-       for(auto &i:mapp){
-        pq.push({i.second, i.first});
-       }
-        string temp="";
-        pair<int, char> prev={0,'#'};
-        while(!pq.empty()){
-            pair<int , char>recc=pq.top();
-            temp+=recc.second;
-            pq.pop();
-            if(prev.first>0){
-                pq.push({prev.first, prev.second});
+        for(int i=0;i<s.length();i++){
+            mapp[s[i]]++;
+            if(mapp[s[i]]>(n+1)/2){
+                return "";
             }
-           recc.first--;
-            prev={recc.first, recc.second};
         }
-        return temp;
-       
+        priority_queue<pair<int , char >>pq;
+        for(auto &i:mapp){
+            pq.push({i.second, i.first});
+        }
+        string temp="";
+       pair<int, char> prev={0,'#'};
+        while(!pq.empty()){
+                pair<int , char>curr=pq.top();
+                pq.pop();
+                temp+=curr.second;
+                if(prev.first>0){
+                    pq.push({prev.first, prev.second});
+                }
+                curr.first--;
+                prev={curr.first, curr.second};
 
+                }
+        
+        return temp;
     }
 };
