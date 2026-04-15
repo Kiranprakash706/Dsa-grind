e900d6bfa96c841174aca16ed41382d5bf87d022
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-04-15 16:01:13 +0530

    Time: 155 ms (30.90%) | Memory: 110.4 MB (38.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -34,9 +34,31 @@ bool helper(vector<int>&nums, int ind, int sum, vector<vector<int>>&dp){
         }
         int m=sum/2;
         
-        vector<vector<int>>dp(n, vector<int>(m + 1, -1));
-        bool ans= helper(nums, n-1,m,dp);
+        vector<vector<int>>dp(n, vector<int>(m + 1, 0));
+        // bool ans= helper(nums, n-1,m,dp);
+        // return dp[n-1][m];
+
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+
+        }
+        if(nums[0]<=m){
+            dp[0][nums[0]]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=m;j++){
+                bool a=false;
+                if(nums[i]<=j){
+                    a=dp[i-1][j-nums[i]];
+                }
+                bool b=dp[i-1][j];
+                dp[i][j]=a||b;
+
+            }
+        }
         return dp[n-1][m];
+
+
        
     }
 };
