b1b3ba50643aa4dda7f89e4d4e09728de5cc6491
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-04-17 16:02:26 +0530

    Time: 3 ms (84.81%) | Memory: 15.5 MB (47.28%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -37,7 +37,36 @@ public:
             return 0;
         }
         int tar=(sum+target)/2;
-        vector<vector<int>>dp(n, vector<int>(tar+1,-1));
-        return helper(n-1, nums, dp, tar);
+        vector<vector<int>>dp(n, vector<int>(tar+1,0));
+        // return helper(n-1, nums, dp, tar);
+
+        //TABULATION 
+
+            if(nums[0]==0){
+                dp[0][0]=2;
+
+            }
+            else{
+                dp[0][0]=1;
+            }
+
+            if(nums[0]!=0 && nums[0]<=tar){
+                dp[0][nums[0]]=1;
+            }
+
+            for(int i=1;i<n;i++){
+                for(int j=0;j<=tar;j++){
+                    int pick=0;
+                    int notpick=dp[i-1][j];
+                    if(j>=nums[i]){
+                        pick=dp[i-1][j-nums[i]];
+
+                    }
+                    dp[i][j]=pick+notpick;
+                }
+
+            }
+            return dp[n-1][tar];
+
     }
 };
