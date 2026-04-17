a2b2a37410ad252e2435df7c3f33b9e8471be081
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-04-17 14:42:53 +0530

    Time: 4 ms (75.97%) | Memory: 15.2 MB (55.46%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int helper(int ind , vector<int>&nums, vector<vector<int>>&dp, int tar){
+        if(ind==0){
+            if(tar==0 && nums[ind]==0){
+                return 2;
+            }
+            else if(tar==0 || nums[ind]==tar){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        if(dp[ind][tar]!=-1){
+            return dp[ind][tar];
+        }
+        else{
+            int pick=0;
+            if(nums[ind]<=tar){
+                pick=helper(ind-1, nums, dp, tar-nums[ind]);
+            }
+            int notpick=helper(ind-1, nums, dp, tar);
+            return dp[ind][tar]=pick+notpick;
+        }
+
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+       
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        if(abs(target) > sum){
+            return 0;
+        }
+        if((sum+target)%2!=0){
+            return 0;
+        }
+        int tar=(sum+target)/2;
+        vector<vector<int>>dp(n, vector<int>(tar+1,-1));
+        return helper(n-1, nums, dp, tar);
+    }
+};
