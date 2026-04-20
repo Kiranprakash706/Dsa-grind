cb27c472fef2997a093acc271282272fa7c3556a
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-04-20 19:05:10 +0530

    Time: 38 ms (35.52%) | Memory: 61.6 MB (9.96%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+int helper(int ind, vector<int>&coins , int sum, vector<vector<long long >>&dp){
+      if(sum==0){
+          return 1;
+      }
+      if(ind==0){
+          if(sum%coins[0]==0){
+              return 1;
+          }
+          else{
+              return 0;
+          }
+      }
+      if(dp[ind][sum]!=-1){
+          return dp[ind][sum];
+      }
+      else{
+          long long  pick=0;
+          if(sum>=coins[ind]){
+              pick=helper(ind, coins , sum-coins[ind],dp);
+          }
+          long long  notpick=helper(ind-1, coins, sum, dp);
+          return dp[ind][sum]=pick+notpick;
+      }
+  }
+    int change(int sum, vector<int>& coins) {
+        int n=coins.size();
+      vector<vector<long long >>dp(n, vector<long long >(sum+1, -1));
+        return helper(n-1, coins, sum, dp);
+        // for(int i=0;i<n;i++){
+        //     dp[i][0]=1;
+        // }
+        // for(int i=0;i<=sum;i++){
+        //     if(i%coins[0]==0){
+        //         dp[0][i]=1;
+        //     }
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=sum;j++){
+        //         long long  pick=0;
+        //         if(j>=coins[i]){
+        //             pick=dp[i][j-coins[i]];
+        //         }
+        //         long long  notpick=dp[i-1][j];
+               
+        //         dp[i][j]=pick+notpick;
+                
+        //     }
+        // }
+        // return dp[n-1][sum];
+        
+    }
+};
