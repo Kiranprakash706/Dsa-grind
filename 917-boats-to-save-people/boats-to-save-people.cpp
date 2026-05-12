e0a4665bdf353a0cff65d1d6b7082dedba477c6d
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-05-12 18:19:16 +0530

    Time: 15 ms (73.14%) | Memory: 45.8 MB (37.01%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         sort(people.begin(), people.end());
         int i=0;
         int j=people.size()-1;
