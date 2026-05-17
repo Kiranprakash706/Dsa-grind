ccee9fa3ab075af978e88d7d7c6d81ee277ce640
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-05-17 18:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>pref(nums.size());
-        vector<int>suff(nums.size());
+        vector<int>ans(nums.size());
         for(int i=0;i<nums.size();i++){
             if(i==0){
-                pref[i]=nums[0];
+                ans[i]=nums[i];
             }
             else{
-                pref[i]=pref[i-1]*nums[i];
+                ans[i]=nums[i]*ans[i-1];
             }
         }
+        int temp=ans[nums.size()-2];
         for(int i=nums.size()-1;i>=0;i--){
             if(i==nums.size()-1){
-                suff[i]=nums[i];
+                ans[i]=temp;
+                temp=nums[i];
+            }
+            else if(i==0){
+                ans[i]=temp;
             }
             else{
-                suff[i]=suff[i+1]*nums[i];
+                ans[i]=ans[i-1]*temp;
+                temp=temp*nums[i];
             }
-        }
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
-                ans.push_back(suff[i+1]);
-            }
-            else if(i==nums.size()-1){
-                ans.push_back(pref[i-1]);
-            }
-            else{
-                ans.push_back(pref[i-1]*suff[i+1]);
-            }
-
-
         }
         return ans;
-        
+
     }
 };
