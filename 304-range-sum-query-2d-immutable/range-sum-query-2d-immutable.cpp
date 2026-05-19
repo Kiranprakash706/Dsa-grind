f10c8b3d7507ee79e7bd4869cc89f62e1317b928
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-05-19 15:30:20 +0530

    Time: 19 ms (78.15%) | Memory: 149.1 MB (87.82%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -2,28 +2,19 @@ class NumMatrix {
 public:
 vector<vector<int>>temp;
     NumMatrix(vector<vector<int>>& matrix) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=1;j<matrix[i].size();j++){
-                matrix[i][j]=matrix[i][j-1]+matrix[i][j];
+        int n=matrix.size();
+        int m=matrix[0].size();
+        temp.resize(n+1, vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                temp[i][j]=matrix[i-1][j-1]+temp[i-1][j]+temp[i][j-1]-temp[i-1][j-1];
             }
         }
-        temp=matrix;
+
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;i++){
-           
-                if(col1==0){
-                    ans+=temp[i][col2];
-                }
-                else{
-                    ans+=(temp[i][col2]-temp[i][col1-1]);
-                }
-
-            
-        }
-        return ans;
+        return temp[row2+1][col2+1]-temp[row1][col2+1]-temp[row2+1][col1]+temp[row1][col1];
     }
 };
 
