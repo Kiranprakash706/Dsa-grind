5109c61f093177f4a8530ee86b69866191dcad40
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-05-25 15:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (91.56%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int>timeline(1001, 0);
+        for(int i=0;i<trips.size();i++){
+            int from=trips[i][1];
+            int to=trips[i][2];
+            timeline[from]+=trips[i][0];
+            timeline[to]-=trips[i][0];
+        }
+        int curr=0;
+        for(int i:timeline){
+            curr+=i;
+            if(curr>capacity){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+//Heap method
+/*class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
         priority_queue<pair<int , int>, vector<pair<int, int >>, greater<pair<int , int >>>pq;
         sort(trips.begin(), trips.end(), [](vector<int>&a, vector<int>&b){
             return a[1]<b[1];
@@ -21,4 +44,4 @@ public:
         }
         return true;
     }
-};
+};*/
