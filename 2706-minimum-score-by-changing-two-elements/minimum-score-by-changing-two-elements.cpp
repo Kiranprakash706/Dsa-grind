f1266c48c7f49ccf4caf6de8f6e0785e149aa522
Kiranprakash706 <hotkiran9470.kp@gmail.com>
2026-06-01 12:17:09 +0530

    Time: 17 ms (39.79%) | Memory: 37.7 MB (98.95%) - LeetSync

--- 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.cpp
+++ 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int minimizeSum(vector<int>& nums) {
-        if(nums.size()<=3){
-            return 0;
-        }
         sort(nums.begin(), nums.end());
-    int n=nums.size()-1;
-       int option1=abs(nums[2]-nums[n]);
-       int option2=abs(nums[n-2]-nums[0]);
-       int option3=abs(nums[n-1]-nums[1]);
-       return min(option1, min(option2, option3));
+        int opta=nums[nums.size()-2]-nums[1];
+        int optb=nums[nums.size()-1]-nums[2];
+        int optc=nums[nums.size()-3]-nums[0];
+        return min(opta,min(optb, optc));
     }
 };
